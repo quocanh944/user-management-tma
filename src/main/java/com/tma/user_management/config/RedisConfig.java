7ec4de390a0b8d00a39f86e15a21bb47c3147dcd
Nguyen Quoc Anh <quocanh944@gmail.com>
2024-03-22 14:06:44 +0700

    Add config to deploy

--- src/main/java/com/tma/user_management/config/RedisConfig.java
+++ src/main/java/com/tma/user_management/config/RedisConfig.java
@@ -4,38 +4,47 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPoolConfig;
 
 @Configuration
 public class RedisConfig {
-    @Value("${redis.host}")
-    private String redisHost;
+//    @Value("${redis.host}")
+//    private String redisHost;
+//
+//    @Value("${redis.port}")
+//    private Integer redisPort;
+//
+//    @Value("${redis.pass}")
+//    private String redisPass;
+//
+//    @Value("${redis.username}")
+//    private String username;
 
-    @Value("${redis.port}")
-    private Integer redisPort;
-
-    @Value("${redis.pass}")
-    private String redisPass;
+//    @Bean
+//    @Primary
+//    public JedisConnectionFactory jedisConnectionFactory() throws Exception {
+//        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(redisHost, redisPort);
+//
+//        redisStandaloneConfiguration.setPassword(RedisPassword.of(redisPass));
+//        redisStandaloneConfiguration.setUsername(username);
+//
+//        JedisConnectionFactory factory = new JedisConnectionFactory(redisStandaloneConfiguration);
+//        factory.setTimeout(0);
+//
+//        return factory;
+//    }
 
     @Bean
-    @Primary
-    public JedisConnectionFactory jedisConnectionFactory() throws Exception {
-        JedisConnectionFactory factory = new JedisConnectionFactory();
-        factory.setHostName(redisHost);
-        factory.setPort(redisPort);
-        if (redisPass != null) {
-            factory.setPassword(redisPass);
-        }
-        factory.setUsePool(true);
-
-        return factory;
-    }
-
-    @Bean
-    public RedisTemplate<String, String> redisTemplate() throws Exception {
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) throws Exception {
         final RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(jedisConnectionFactory());
+        redisTemplate.setConnectionFactory(factory);
 
         return redisTemplate;
     }
